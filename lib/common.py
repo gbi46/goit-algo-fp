a2f291c0ba2e128feb430973d0b499ae5e21479e
ivcoder <vank300828@gmail.com>
2025-09-23 20:31:35 +0200

    demonstrate linked list reversing

--- lib/common.py
+++ lib/common.py
@@ -7,7 +7,7 @@ def print_task_header(task_number: int):
 
     for i in range(6):
         if i == 3:
-            print(Fore.GREEN + "=" + Fore.YELLOW + " " * 8 + f"Task {task_number}" + Fore.GREEN + " " * 9 + "=")
+            print(Fore.GREEN + "=" + Fore.YELLOW + " " * 8 + f"Task {task_number}" + Fore.GREEN + " " * 10 + "=")
         else:
             print(Fore.GREEN + "=                        =")
     print(Fore.GREEN + "==========================")
---
+++ main.py
@@ -0,0 +1,19 @@
+from lib.common import print_task_header
+from lib.linked_list import LinkedList
+
+def main():
+    print_task_header(1)
+    linked_list = LinkedList()
+    
+    for value in [3, 1, 4, 1, 5, 9, 2, 6, 5]:
+        linked_list.append(value)
+
+    print("Original Linked List:")
+    linked_list.print_list()
+
+    linked_list.reverse()
+    print("Reversed Linked List:")
+    linked_list.print_list()
+
+if __name__ == "__main__":
+    main()
