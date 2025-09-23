b4ae1697cb639e2c14ee7ad0f5963df6cb9fecb4
ivcoder <vank300828@gmail.com>
2025-09-23 20:21:51 +0200

    add print_task_header common function

---
+++ lib/common.py
@@ -0,0 +1,13 @@
+from colorama import Fore, init
+
+init(autoreset=True)
+
+def print_task_header(task_number: int):
+    print(Fore.GREEN + "==========================")
+
+    for i in range(6):
+        if i == 3:
+            print(Fore.GREEN + "=" + Fore.YELLOW + " " * 8 + f"Task {task_number}" + Fore.GREEN + " " * 9 + "=")
+        else:
+            print(Fore.GREEN + "=                        =")
+    print(Fore.GREEN + "==========================")
